6436fbd814600158de5783bf4574b4814b4fd9ef
svillar <villar.mucasan@gmail.com>
2024-07-26 01:15:09 -0300

    add structure to put content into select HTML element throughout javascript (needs to be fixed, as it is broken)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-function inputDropButtonData(){
-
-}
-function setDropButtonList(){
-
-}
 // this is just a method created for testing the read file
-async function displayJsonFile(){
-    data = await fetch(
-        'http://127.0.0.1:5500/src/json/algorithms.json'
-    );
-    alert(data);
+async function fetchJsonFile(){
+    // this res variable corresponds to the response of the try and catch handling exceptions
+    try{
+        // tries to fetch the JSON data that is stored in the localhost server
+        res = await fetch('http://127.0.0.1:5500/src/json/algorithms.json');
+        if (!res.ok){
+            throw new Error("HTTPS error! Status: ${response.status}");
+        }
+        return res.json();
+    } catch(error){
+        throw new Error("HTTP request couldn't fetch from algorithms.json file AND/OR file doesn't exist anymore.");
+    }
 }
+function inputDropButtonData(){
+    const data = fetchJsonFile();
+    const btnDropDown = document.querySelector("#btnDropDown");
+    setDropButtonList(data,btnDropDown);
+}
+function setDropButtonList(data,btnDropDown){
+    var i = data[0]["algorithms"].length;
+    var j = 0;
+    while(j <= i){
+        createObjectOption(btnDropDown, data[0]["algorithms"][j]);
+        j++;
+    }
+}
+function createObjectOption(btnDropButton, optionName){
+    var option = document.createElement('option');
+    option.value = optionName;
+    option.innerHTML = optionName; 
+    btnDropButton.appendChild(option);
+}
+document.addEventListener("DOMContentLoaded", inputDropButtonData());
+
+
+
+
+
