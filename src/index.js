6e330cee3db85e9a1efb26996496a91f0843a786
svillar <villar.mucasan@gmail.com>
2024-07-26 23:41:17 -0300

    fix issues with loading algorithms inside drop down button

--- src/index.js
+++ src/index.js
@@ -5,23 +5,26 @@ async function fetchJsonFile(){
         // tries to fetch the JSON data that is stored in the localhost server
         res = await fetch('http://127.0.0.1:5500/src/json/algorithms.json');
         if (!res.ok){
-            throw new Error("HTTPS error! Status: ${response.status}");
+            throw new Error("HTTPS error! Status: ${res.status}");
         }
         return res.json();
     } catch(error){
         throw new Error("HTTP request couldn't fetch from algorithms.json file AND/OR file doesn't exist anymore.");
     }
 }
-function inputDropButtonData(){
-    const data = fetchJsonFile();
+async function inputDropButtonData(){
+    // fetch the data, and use the await keyword to automatically make the promise interactable
+    const data = await fetchJsonFile();
+    // create DOM object for interacting with select button
     const btnDropDown = document.querySelector("#btnDropDown");
+    console.log(data["algorithms"]);
     setDropButtonList(data,btnDropDown);
 }
 function setDropButtonList(data,btnDropDown){
-    var i = data[0]["algorithms"].length;
+    var i = data["algorithms"].length;
     var j = 0;
     while(j <= i){
-        createObjectOption(btnDropDown, data[0]["algorithms"][j]);
+        createObjectOption(btnDropDown, data["algorithms"][j]);
         j++;
     }
 }
