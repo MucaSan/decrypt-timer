c631a9762df7b08d1fbf6f17a660258693c01211
svillar <villar.mucasan@gmail.com>
2024-09-12 22:13:43 -0300

    try to fix API not calling data, but was unsucessful

--- backend/api/controllers/caesar_controller.go
+++ backend/api/controllers/caesar_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"api/api/models"
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -12,10 +13,16 @@ import (
 func EncryptDescription(c *gin.Context) {
 	var caesarEncrypt models.Caesar
 	c.ShouldBindBodyWithJSON(&caesarEncrypt)
+	if err := models.ValidateCaesar(caesarEncrypt); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error: ": err.Error()})
+		return
+	}
 	caesarEncrypt.SecretKey = strings.ToUpper(caesarEncrypt.SecretKey)
 	for i, letter := range models.EnglishAlphabet {
 		if string(letter) == caesarEncrypt.SecretKey {
 			c.JSON(http.StatusOK, gin.H{"Encrypted message": caesarCipher.Encrypt(caesarEncrypt.Description, uint(i+1))})
+			fmt.Println("Passing here!")
 		}
 	}
+
 }
--- backend/api/models/caesar.go
+++ backend/api/models/caesar.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"gopkg.in/validator.v2"
 	"gorm.io/gorm"
 )
 
@@ -8,6 +9,13 @@ const EnglishAlphabet string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 type Caesar struct {
 	gorm.Model
-	Description string `json: "description" validate:"max=200, regexp=^[a-zA-Z]*$`
-	SecretKey   string `json: "secretKey" validate:"max=1, regexp=^[a-zA-Z]*$`
+	Description string `json: "description" validate:"required max=200, regexp=^[a-zA-Z]*$"`
+	SecretKey   string `json: "secretKey" validate:"required len=1, regexp=^[a-zA-Z]*$"`
+}
+
+func ValidateCaesar(c Caesar) error {
+	if err := validator.Validate(c); err != nil {
+		return err
+	}
+	return nil
 }
